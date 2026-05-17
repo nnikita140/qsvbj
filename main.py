3d4eba2b25d519a0b6d3db28ec727ab0126817e2
nnikita140 <nevedomskiy21@bk.ru>
2026-05-17 12:42:00 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,38 +1,95 @@
-from dbm import sqlite3
-from telebot import TeleBot
-from io import BytesIO
-import db
-from image_manager import init_board
-
-BASE_URL = 'images'
-TOKEN = ''
-bot = TeleBot(TOKEN)
+from time import sleep
 
 
-def converter_to_bytes(board):
-    image_io = BytesIO()
-    board.save(image_io, 'PNG')
-    image_io.seek(0)
-    return image_io
-
-@bot.message_handler(commands=['create_game'])
-def create_game(message):
-    global game_id
-    game_id = db.create_game(message.from_user.id)
-    bot.send_message(message.from_user.id, 'Вы создали игру с id:' + game_id)
-    board, init_pos = init_board()
-    db.create_game_state(game_id, 0, init_pos)
-    bot.send_photo(message.chat.id, converter_to_bytes(board))
+def creature_board():
+    return([['.', '.', '.'],
+            ['.', '.', '.'],
+            ['.', '.', '.']])
 
 
-@bot.message_handler(commands=['join_game'])
-def join_game(message):
-    _, game_id = message.text.split(' ')
-    if db.game_full(game_id):
-        bot.send_message(message.chat.id, 'Игра уже началась')
-        return
-    db.join_game(message.from_user.id, game_id)
-    bot.send_message(message.chat.id, 'Вы присоединились к игре с id: ' + message.text)
-    white_user_id = db.get_user_id(game_id, 'white')
-    if white_user_id:
-        bot.send_message(white_user_id[0], 'К игре присоединился игрок' + message.from_user.first_name)
+def print_board(board):
+    print('  0 1 2')
+    for i, row in enumerate(board):
+        print(i, ' '.join(row))
+
+
+def row_win(board, player):
+    for x in range(len(board)):
+        win = True
+        for y in range(len(board)):
+            if board[x][y] != player:
+                win = False
+                continue
+            if win:
+                return win
+        return False
+
+
+def col_win(board, player):
+    for x in range(len(board)):
+        win = True
+
+
+        for y in range(len(board)):
+            if board[y][x] != player:
+                win = False
+                continue
+            if win:
+                return win
+        return False
+
+
+def diag_win(board, player):
+    win = True
+    y = 0
+    for x in range(len(board)):
+        if board[x][y] != player:
+            win = False
+            if win:
+                return win
+            win = True
+            if win:
+                for x in range(len(board)):
+                    y = len(board) - 1 - x
+                    if board[x][y] != player:
+                        win = False
+            return False
+        
+
+def evaluate(board):
+    winner = False
+    for player in ['X', 'O']:
+        if (row_win(board, player) or
+            col_win(board, player) or
+            diag_win(board, player)):
+            winner = player
+        return winner
+
+
+def place_board(col, row, sign, board):
+    if col > 2 or row > 2 or row < 0 or col <0:
+        return False, 'Место за пределами доски'
+    if board[col][row] != '.':
+        return False, 'Место занято!'
+    board[col][row] = sign
+    return True, ''
+
+
+def play_game():
+    board, winner = creature_board(), False
+    print_board(board)
+    sleep(1)
+    sign = 'X'
+    while not winner:
+        col = int(input('Выберите ряд: '))
+        row = int(input('Выберите столбец: '))
+        success, error = place_board(col, row, sign, board)
+        print_board(board)
+        if not success:
+            print(error)
+            continue
+        sign = '0' if sign == 'X' else 'X'
+        winner = evaluate(board)
+    print(winner)
+
+play_game()
